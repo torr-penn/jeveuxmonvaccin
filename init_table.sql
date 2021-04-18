3e19d97745e206de3c2022b7618d0b1ffecdaa92
gtanguy <gwendal.tanguy@gmail.com>
2021-04-18 18:34:02 +0200

    adding script to parse center list before db upload

--- init_table.sql
+++ init_table.sql
@@ -1,103 +1,108 @@
 CREATE TABLE IF NOT EXISTS vaccine_center
 (
-    id BIGSERIAL PRIMARY KEY,
-    center_id int,
-	vaccine_id int,
-    primary_link character varying ( 200),
+    id              BIGSERIAL PRIMARY KEY,
+    center_id       int,
+    vaccine_id      int,
+    primary_link    character varying(200),
     result_topology int,
-    provider character varying ( 20),
-    lastok timestamp without time zone default now (),
-lastcheck timestamp without time zone default now (),
-    lastphonecheck timestamp without time zone default now (),
-    date_in date not null default CURRENT_DATE,
-    status int default 3,
-    token int default 0,
-    init_token int default 10,
-    msinterval int default 120000,
-    nbsuccess int default 0,
-    total int default 0,
-params type character varying(500)
+    provider        character varying(20),
+    lastok          timestamp without time zone default now(),
+    lastcheck       timestamp without time zone default now(),
+    lastphonecheck  timestamp without time zone default now(),
+    date_in         date not null               default CURRENT_DATE,
+    status          int                         default 3,
+    token           int                         default 0,
+    init_token      int                         default 10,
+    msinterval      int                         default 120000,
+    nbsuccess       int                         default 0,
+    total           int                         default 0,
+    params          character varying(500)
 );
+
 
 CREATE TABLE IF NOT EXISTS vaccine_phone
 (
-    id
-    BIGSERIAL
-    PRIMARY
-    KEY,
-    phonesalt
-    character
-    varying
-(
-    50
-) UNIQUE,
-    centerid int references vaccine_center
-(
-    id
-) on delete cascade,
-    lastok timestamp
-  without time zone,
-    lastcheck timestamp
-  without time zone,
-    date_in date not null default CURRENT_DATE,
-    msinterval int default 120000,
-    total int default 0,
-    nbsuccess int default 0,
-    CONSTRAINT salt_unique UNIQUE
-(
-    phonesalt
-));
+    id         BIGSERIAL PRIMARY KEY,
+    phonesalt  character varying(50) UNIQUE,
+    centerid   int references vaccine_center (id) on delete cascade,
+    lastok     timestamp without time zone,
+    lastcheck  timestamp without time zone,
+    date_in    date not null default CURRENT_DATE,
+    msinterval int           default 120000,
+    total      int           default 0,
+    nbsuccess  int           default 0,
+    CONSTRAINT salt_unique UNIQUE (phonesalt)
+);
 
+alter table vaccine_center
+    add constraint uniquecenter_vaccine_center UNIQUE (center_id, vaccine_id);
 
 drop table vaccine_phone;
 drop table vaccine_center;
 
+-- various maintenance operations below
 
-COPY vaccine_center(center_id,provider,vaccine_id,primary_link)
+
+COPY vaccine_center (center_id, provider, vaccine_id, primary_link)
     FROM '~/covid/center/load_center_sql.csv'
     WITH DELIMITER ';'
 ;
 
 
 ^
-torr-penn::DATABASE=> insert into vaccine_center (id,center_id) values (0,0);
+torr-penn::DATABASE=>
+insert into vaccine_center (id, center_id)
+values (0, 0);
 INSERT
-0 1
-
-heroku psql -a torr-penn -c "\COPY  vaccine_center(center_id,provider,vaccine_id,primary_link) 
+    0 1
+    heroku psql -a torr-penn - c "\COPY  vaccine_center(center_id,provider,vaccine_id,primary_link)
 FROM '~/covid/center/load_center_sql.csv'
 WITH DELIMITER ';'
 ;"
 
-\COPY  vaccine_center(center_id,provider,vaccine_id,result_topology,params,primary_link) 
-FROM '/tmp/load_center_sql.csv' WITH DELIMITER ';' ;
+\COPY  vaccine_center(center_id,provider,vaccine_id,result_topology,params,primary_link)
+FROM '/tmp/load_center_sql.csv'
+WITH DELIMITER ';';
 
-update vaccine_center set result_topology=2, status=1, params='2678861:449685:175124' where center_id=1009;
- alter table vaccine_center add constraint uniquecenter_vaccine_center UNIQUE(center_id,vaccine_id);
-update vaccine_center set result_topology=2, status=1,vaccine_id=2, params='2575758:429756-417446-416771-433205-436456-436455:167193' where center_id=1277;
-
-alter table vaccine_center add column params character varying(200);
-
-
-
-
+update vaccine_center
+set result_topology=2,
+    status=1,
+    params='2678861:449685:175124'
+where center_id = 1009;
+update vaccine_center
+set result_topology=2,
+    status=1,
+    vaccine_id=2,
+    params='2575758:429756-417446-416771-433205-436456-436455:167193'
+where center_id = 1277;
 
 \COPY  center_temp(center_id,provider,vaccine_id,result_topology,params) FROM '/tmp/extra_29.txt' WITH DELIMITER ';' ;
 \COPY  center_temp(center_id,provider,vaccine_id,result_topology,params) FROM '/tmp/extra_22.txt' WITH DELIMITER ';' ;
 
-delete from vaccine_center where center_id=925 and vaccine_id=1;
- update vaccine_center set vaccine_id=2 where center_id=1438 and vaccine_id=5;
-update vaccine_center set vaccine_id=2 where center_id=2001 and vaccine_id=5;
- update vaccine_center set vaccine_id=2 where center_id=422 and vaccine_id=5;
-update vaccine_center set vaccine_id=2 where center_id=56 and vaccine_id=5;
-
-
-
-update vaccine_center set result_topology=b.result_topology, params=b.params, status=2 from center_temp b where vaccine_center.vaccine_id=b.vaccine_id and vaccine_center.center_id=b.center_id;
-
-alter table vaccine_center alter column params type character varying(500);
-alter table center_temp alter column 
-params type character varying(500);
-
-
-
+delete
+from vaccine_center
+where center_id = 925
+  and vaccine_id = 1;
+update vaccine_center
+set vaccine_id=2
+where center_id = 1438
+  and vaccine_id = 5;
+update vaccine_center
+set vaccine_id=2
+where center_id = 2001
+  and vaccine_id = 5;
+update vaccine_center
+set vaccine_id=2
+where center_id = 422
+  and vaccine_id = 5;
+update vaccine_center
+set vaccine_id=2
+where center_id = 56
+  and vaccine_id = 5;
+update vaccine_center
+set result_topology=b.result_topology,
+    params=b.params,
+    status=2
+from center_temp b
+where vaccine_center.vaccine_id = b.vaccine_id
+  and vaccine_center.center_id = b.center_id;
---
+++ scripts/processCenter.sh
@@ -0,0 +1,172 @@
+#!/bin/bash 
+
+WDIR=/home/gtanguy/covid/center
+
+FILE="centres-vaccination.csv"
+
+IDX=0
+DEPARTEMENT=29
+
+
+FILEO="$WDIR/center/filtered_center${IDX}.csv"
+echo -n ""> load_center_sql.csv
+
+MYDATE=`date`
+	nbcenterseen=0
+	nbcenterdeptseen=0
+	nbcenterdeptadded=0
+
+
+function process_department(){
+	DEPARTEMENT=$1
+	echo "process department : $DEPARTEMENT.csv"
+	echo -n ""> $DEPARTEMENT.csv
+	process_file $SWDIR/$FILE
+}
+
+
+function process_file() {
+	echo "reading file $1"
+	RES=""
+ 	process_line < $WDIR/$FILE
+	echo "total center $nbcenterseen"
+	echo "total center in departement $DEPARTEMENT  :   $nbcenterdeptseen"
+	echo "total center valid for departement $DEPARTEMENT  :   $nbcenterdeptadded"
+	echo "file generated $DEPARTEMENT.csv size:"
+	cat $DEPARTEMENT.csv|wc -l
+}
+function extra_info() {
+   GID=$1
+
+   EXTRANB=`cat extra_$DEPARTEMENT.txt|grep -o "^$GID;.*"|wc -l`
+   EXTRA1=""
+   EXTRA2=""
+   EXTRA3=""
+   
+
+if [ "$EXTRANB" = "1" ]; then
+   EXTRA1=`cat extra_$DEPARTEMENT.txt|grep -o "^$GID;.*"`
+   EXTRA2=""
+   EXTRA3=""
+else
+
+if [ "$EXTRANB" = "2" ]; then
+   EXTRA1=`cat extra_$DEPARTEMENT.txt|grep -o "^$GID;.*"|awk 'NR==1'`
+   EXTRA2=`cat extra_$DEPARTEMENT.txt|grep -o "^$GID;.*"|awk 'NR==2'`
+   EXTRA3=""
+else
+   EXTRA1=`cat extra_$DEPARTEMENT.txt|grep -o "^$GID;.*"|awk 'NR==1'`
+   EXTRA2=`cat extra_$DEPARTEMENT.txt|grep -o "^$GID;.*"|awk 'NR==2'`
+   EXTRA3=`cat extra_$DEPARTEMENT.txt|grep -o "^$GID;.*"|awk 'NR==3'`
+fi
+
+fi
+
+   #echo "for center : $GID I found : $EXTRA"
+	
+}
+function output_to_file(){
+					if [ "$adr_num" = "" ]; then
+					  monadresse="$adr_voie"
+					else 
+					  monadresse="$adr_num, $adr_voie"
+					fi
+					if [ "$EXTRANB" = "0" ]; then
+						return;
+					fi
+					if [ "$EXTRANB" = "1" ]; then
+
+		      				echo -e "$nom;$monadresse;$com_nom;$com_cp;$date_ouverture;$date_fermeture;$rdv_site_web;$EXTRA1"   >> $DEPARTEMENT.csv
+						echo -e "$EXTRA1;$rdv_site_web" >> load_center_sql.csv
+				        else
+				               if [ "$EXTRANB" = "2" ]; then
+		      			 		echo -e "$nom;$monadresse;$com_nom;$com_cp;$date_ouverture;$date_fermeture;$rdv_site_web;$EXTRA1"   >> $DEPARTEMENT.csv
+		      					echo -e "$nom;$monadresse;$com_nom;$com_cp;$date_ouverture;$date_fermeture;$rdv_site_web;$EXTRA2"   >> $DEPARTEMENT.csv
+						echo -e "$EXTRA1;$rdv_site_web" >> load_center_sql.csv
+						echo -e "$EXTRA2;$rdv_site_web" >> load_center_sql.csv
+
+						else
+		      					echo -e "$nom;$monadresse;$com_nom;$com_cp;$date_ouverture;$date_fermeture;$rdv_site_web;$EXTRA1"   >> $DEPARTEMENT.csv
+		      					echo -e "$nom;$monadresse;$com_nom;$com_cp;$date_ouverture;$date_fermeture;$rdv_site_web;$EXTRA2"   >> $DEPARTEMENT.csv
+		      					echo -e "$nom;$monadresse;$com_nom;$com_cp;$date_ouverture;$date_fermeture;$rdv_site_web;$EXTRA3"   >> $DEPARTEMENT.csv
+						echo -e "$EXTRA1;$rdv_site_web" >> load_center_sql.csv
+						echo -e "$EXTRA2;$rdv_site_web" >> load_center_sql.csv
+						echo -e "$EXTRA3;$rdv_site_web" >> load_center_sql.csv
+					     	fi
+					 fi
+
+}
+
+
+function process_line() {
+while IFS=\; read gid nom arrete_pref_numero xy_precis id_adr adr_num adr_voie com_cp com_insee com_nom lat_coor1 long_coor1 structure_siren structure_type structure_rais structure_num structure_voie structure_cp structure_insee structure_com _userid_creation _userid_modification _edit_datemaj lieu_accessibilite rdv_lundi rdv_mardi rdv_mercredi rdv_jeudi rdv_vendredi rdv_samedi rdv_dimanche rdv date_fermeture date_ouverture rdv_site_web rdv_tel rdv_tel2 rdv_modalites rdv_consultation_prevaccination centre_svi_repondeur centre_fermeture reserve_professionels_sante
+do  
+
+        let nbcenterseen++
+	RESBUFFER=""
+	DEPT_CK=`echo $com_cp |sed -e 's/'${DEPARTEMENT}'[0-9][0-9][0-9]/FOUND/' `
+	if [ "$DEPT_CK" = "FOUND" ]; then
+       let nbcenterdeptseen++
+
+		RESBUFFER="$com_cp;$com_nom;$nom;"
+		if [ "$date_ouverture" != "" ]; then
+			if [ "$rdv_modalites" = "" ]; then
+	 			if [ "$rdv_site_web" != "" ]; then
+					if [ "$date_fermeture" = "" ]; then
+						extra_info $gid	
+					if [ "$EXTRANB" = "0" ]; then
+						echo "----------INCOMPLETE : $gid;$rdv_site_web;$com_nom;$com_cp" 
+					fi
+					output_to_file 
+						RESBUFFER="$RESBUFFER;ADDED"
+       					let nbcenterdeptadded++
+					else
+				 		viewdate=$(date -d $date_fermeture +%s)
+				 		limitdate=$(date -d "+7 days"  "+%s")
+			 			mylimit=$(date -d "+7 days" "+%Y-%m-%d" )
+
+				 		if [ $limitdate -ge $viewdate ]; then
+						    RESBUFFER="$RESBUFFER;FILTERED;closedate:$date_fermeture"
+			          	else 
+						    RESBUFFER="$RESBUFFER;ADDED"
+       						let nbcenterdeptadded++
+							extra_info $gid	
+					if [ "$EXTRANB" = "0" ]; then
+						echo "----------INCOMPLETE : $gid;$rdv_site_web;$com_nom;$com_cp" 
+					fi
+					output_to_file
+
+
+							RESBUFFER="$com_cp;$com_nom\n$RESBUFFER"
+
+						fi
+				 	fi  
+	      		else
+				     RESBUFFER="$RESBUFFER;FILTERED;noweb"
+				fi
+			else
+				RESBUFFER="$RESBUFFER;FILTERED;special"
+			fi
+		else
+				RESBUFFER="$RESBUFFER;FILTERED;noopendate"
+		fi
+
+		echo  "$RESBUFFER"
+	fi
+	structure_voie="VOIE"
+	structure_num="NUM"
+
+
+done
+
+}
+
+
+process_department 29
+process_department 22
+process_department 35
+process_department 56
+
+
+
+
