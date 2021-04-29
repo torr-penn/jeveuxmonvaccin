05f8eca493ba40ed89ebee652140ef35d7e6cd1a
gtanguy <gwendal.tanguy@gmail.com>
2021-04-29 13:16:26 +0200

    mises à jour des infos centres

--- init_table.sql
+++ init_table.sql
@@ -20,6 +20,21 @@ CREATE TABLE IF NOT EXISTS vaccine_center
 );
 
 
+CREATE TABLE IF NOT EXISTS vaccine_center_infos
+(
+    id          BIGSERIAL PRIMARY KEY,
+    name        character varying(200),
+    address     character varying(400),
+    city        character varying(200),
+    zipcode     int,
+    open_date   date not null default CURRENT_DATE,
+    close_date  date,
+    center_id   int,
+    vaccine_id  int,
+    platform_id int
+);
+
+
 CREATE TABLE IF NOT EXISTS vaccine_phone
 (
     id         BIGSERIAL PRIMARY KEY,
@@ -49,7 +64,9 @@ COPY vaccine_center (center_id, provider, vaccine_id, primary_link)
 ;
 
 
-^
+\COPY  vaccine_center_infos (name,address,city,zipcode,open_date,close_date,center_id,vaccine_id,platform_id) FROM '/tmp/bretagne.csv' WITH (DELIMITER ';', NULL '');
+
+
 torr-penn::DATABASE=>
 insert into vaccine_center (id, center_id)
 values (0, 0);
@@ -64,19 +81,52 @@ WITH DELIMITER ';'
 FROM '/tmp/load_center_sql.csv'
 WITH DELIMITER ';';
 
-update vaccine_center set result_topology=2, status=1, params='2678861:449685:175124' where center_id = 1009;
-update vaccine_center set result_topology=2, status=1, vaccine_id=2, params='2575758:429756-417446-416771-433205-436456-436455:167193' where center_id = 1277;
+update vaccine_center
+set result_topology=2,
+    status=1,
+    params='2678861:449685:175124'
+where center_id = 1009;
+update vaccine_center
+set result_topology=2,
+    status=1,
+    vaccine_id=2,
+    params='2575758:429756-417446-416771-433205-436456-436455:167193'
+where center_id = 1277;
 
 \COPY  center_temp(center_id,provider,vaccine_id,result_topology,params) FROM '/tmp/extra_29.txt' WITH DELIMITER ';' ;
 \COPY  center_temp(center_id,provider,vaccine_id,result_topology,params) FROM '/tmp/extra_22.txt' WITH DELIMITER ';' ;
 
-delete from vaccine_center where center_id = 925 and vaccine_id = 1;
-update vaccine_center set vaccine_id=2 where center_id = 1438 and vaccine_id = 5;
-update vaccine_center set vaccine_id=2 where center_id = 2001 and vaccine_id = 5;
-update vaccine_center set vaccine_id=2 where center_id = 422 and vaccine_id = 5;
-update vaccine_center set vaccine_id=2 where center_id = 56 and vaccine_id = 5;
-update vaccine_center set result_topology=b.result_topology, params=b.params, status=2
-from center_temp b where vaccine_center.vaccine_id = b.vaccine_id and vaccine_center.center_id = b.center_id;
+
+\COPY  center_temp(center_id,provider,vaccine_id,result_topology,params) FROM '/tmp/extra_22.txt' WITH DELIMITER ';' ;
+
+
+delete
+from vaccine_center
+where center_id = 925
+  and vaccine_id = 1;
+update vaccine_center
+set vaccine_id=2
+where center_id = 1438
+  and vaccine_id = 5;
+update vaccine_center
+set vaccine_id=2
+where center_id = 2001
+  and vaccine_id = 5;
+update vaccine_center
+set vaccine_id=2
+where center_id = 422
+  and vaccine_id = 5;
+update vaccine_center
+set vaccine_id=2
+where center_id = 56
+  and vaccine_id = 5;
+update vaccine_center
+set result_topology=b.result_topology,
+    params=b.params,
+    status=2
+from center_temp b
+where vaccine_center.vaccine_id = b.vaccine_id
+  and vaccine_center.center_id = b.center_id;
 
 
 
